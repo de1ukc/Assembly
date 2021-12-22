6fdf29053695fcec8da4398b97a0222f977e16f4
de1ukc <Trapper.MD@mail.ru>
2021-12-22 20:10:24 +0300

    Semest done

---
+++ lab_6/lab06/bit_ops.cpp
@@ -0,0 +1,24 @@
+#include "bit_ops.h"
+
+// Возвращает бит номер n числа x.
+// Предполагается 0 <= n <= 31
+unsigned get_bit(unsigned x,
+                 unsigned n) {
+    // YOUR CODE HERE
+    // Взврат -1 бессмысленный и сделан для правильной компиляции
+    return -1;
+}
+// Выставляет значение v в бите номер n
+// Предполагается что 0 <= n <= 31 и v равно 0 или 1
+void set_bit(unsigned * x,
+             unsigned n,
+             unsigned v) {
+    // YOUR CODE HERE
+}
+// Меняет значение бита номер n на противоположное.
+// Предполагается что 0 <= n <= 31
+void flip_bit(unsigned * x,
+              unsigned n) {
+    // YOUR CODE HERE
+}
+
---
+++ lab_6/lab06/interactive_hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[]) {
+    std::string name;
+    std::cout  << "Introduce yourself\n";
+    std::cin >> name;
+    std::cout << "O Hi, " << name << "\n i'm just wanna to say hello!\n Have a nice day.\n";
+
+    return 0;
+}
